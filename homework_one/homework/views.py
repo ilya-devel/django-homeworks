44e04768b00ed647ee1b0319804176031f61af48
ilias <ilias@prime.ru>
2023-11-28 12:47:08 +0500

    add view index

--- homework_one/homework/views.py
+++ homework_one/homework/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import logging
+from functools import wraps
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
+
+def write_log(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        logger.info(f'{func.__name__} launched')
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@write_log
+def index(request):
+    html = """
+    <h1>HOME PAGE</h1>
+    <p>This is simple home page for example</p>
+"""
+    return HttpResponse(html)
