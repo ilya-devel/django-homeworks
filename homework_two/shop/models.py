b20efbaab59eccf165b98f05ec761782d036e1fa
ilias <ilias@prime.ru>
2023-12-01 15:12:32 +0500

    homework comleted ver1

---
+++ homework_two/shop/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.core.validators import RegexValidator
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(
+        # Validators should be a list
+        validators=[phone_regex], max_length=17, blank=True)
+    address = models.CharField(max_length=200)
+    reg_data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'Customer: {self.name}\nEmail: {self.email}\nPhone: {self.phone_number}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    count = models.IntegerField()
+    add_data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'Name: {self.name}\nPrice: {self.price}'
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    full_price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    create_date = models.DateTimeField(auto_now_add=True)
+    is_done = models.BooleanField(default=False)
+
+    def set_full_price(self, *args, **kwargs):
+        self.full_price = sum([product.price for product in self.products.all()])
+        self.save()
+        
+    def __str__(self) -> str:
+        return f'''
+Order by {self.customer.name}:
+From: {self.create_date}
+Products list:
+\t{"\n\t".join(
+    [str(prod).replace('\n','\t') for prod in self.products.all()]
+    ) if self.products.all() else "empty list"}
+
+Full price: {self.full_price}
+'''
